521f943bc45932427a9585600af1df0b87ce0637
Daniel Waddington <d.waddington@samsung.com>
2014-11-18 08:51:14 -0800

    removed namespace from cycle routines

--- lib/libexo/exo/cycles.h
+++ lib/libexo/exo/cycles.h
@@ -1,30 +1,30 @@
 /*
-   eXokernel Development Kit (XDK)
+  eXokernel Development Kit (XDK)
 
-   Based on code by Samsung Research America Copyright (C) 2013
+  Based on code by Samsung Research America Copyright (C) 2013
  
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+  The GNU C Library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+  The GNU C Library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General Public
+  License along with the GNU C Library; if not, see
+  <http://www.gnu.org/licenses/>.
 
-   As a special exception, if you link the code in this file with
-   files compiled with a GNU compiler to produce an executable,
-   that does not cause the resulting executable to be covered by
-   the GNU Lesser General Public License.  This exception does not
-   however invalidate any other reasons why the executable file
-   might be covered by the GNU Lesser General Public License.
-   This exception applies to code released by its copyright holders
-   in files containing the exception.  
+  As a special exception, if you link the code in this file with
+  files compiled with a GNU compiler to produce an executable,
+  that does not cause the resulting executable to be covered by
+  the GNU Lesser General Public License.  This exception does not
+  however invalidate any other reasons why the executable file
+  might be covered by the GNU Lesser General Public License.
+  This exception applies to code released by its copyright holders
+  in files containing the exception.  
 */
 
 /*
@@ -43,53 +43,45 @@
 
 #include "types.h"
 
-#if defined(__cplusplus)
-namespace Exokernel
-{
-#endif
 
 #if defined(__amd64__)
 
-  /** 
-   * Reads complete 40 bit counter into 64 bit value.
-   * @return the read value. 
-   */
-  INLINE cpu_time_t rdtsc() {
-    unsigned a, d;
-    asm volatile("lfence"); // should be mfence for AMD
-    asm volatile("rdtsc" : "=a" (a), "=d" (d)); 
-    return ((unsigned long long) a) | (((unsigned long long) d) << 32);    
-  }
+/** 
+ * Reads complete 40 bit counter into 64 bit value.
+ * @return the read value. 
+ */
+INLINE cpu_time_t rdtsc() {
+  unsigned a, d;
+  asm volatile("lfence"); // should be mfence for AMD
+  asm volatile("rdtsc" : "=a" (a), "=d" (d)); 
+  return ((unsigned long long) a) | (((unsigned long long) d) << 32);    
+}
 
-  /** 
-   * Reads low 32 bits of the timestamp counter.
-   * @return the read value. 
-   */
-  INLINE uint32_t rdtsc_low() {
-    uint32_t a;
-    asm volatile("lfence"); // should be mfence for AMD
-    asm volatile("rdtsc" : "=a" (a)); //, "=d" (d)); 
-    return a;
-  }
+/** 
+ * Reads low 32 bits of the timestamp counter.
+ * @return the read value. 
+ */
+INLINE uint32_t rdtsc_low() {
+  uint32_t a;
+  asm volatile("lfence"); // should be mfence for AMD
+  asm volatile("rdtsc" : "=a" (a)); //, "=d" (d)); 
+  return a;
+}
 
 
 #elif defined(__i386)
 
-  /** 
-   * Reads complete 40 bit counter into 64 bit value.
-   * @return the read value. 
-   */
-  INLINE cpu_time_t rdtsc() {
-    unsigned long long ret;
-    asm volatile("lfence"); // should be mfence for AMD
-    asm volatile("rdtsc": "=A" (ret));
-    return ret;
-  }
-
-#endif
-
-#if defined(__cplusplus)
+/** 
+ * Reads complete 40 bit counter into 64 bit value.
+ * @return the read value. 
+ */
+INLINE cpu_time_t rdtsc() {
+  unsigned long long ret;
+  asm volatile("lfence"); // should be mfence for AMD
+  asm volatile("rdtsc": "=A" (ret));
+  return ret;
 }
+
 #endif
 
 
@@ -100,7 +92,6 @@ namespace Exokernel
  * @return the TSC frequency in megahertz.
  */
 double get_tsc_frequency_in_mhz(void); 
-
 
 
 #else
