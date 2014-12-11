a205995bd76240e5d6abb66e0238f169eb2340e8
Daniel Waddington <d.waddington@samsung.com>
2014-12-10 18:39:37 -0800

    added to build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ RESET='\e[0m'
 width=50
 
 function build_dir {
-printf "%-$width.${width}s" "[BUILD] library ($1) ..."
+printf "%-$width.${width}s" "[BUILD] library ($1) ... "
 cd ${XDK_HOME}/$2 ; make clean &>> ${XDK_HOME}/build.log; make  &>> ${XDK_HOME}/build.log
-[ $? -eq 0 ] && echo -e "${GREEN}OK${RESET}" || echo -e "${RED}FAILED${RESET}"
+[ $? -eq 0 ] && echo -e " ${GREEN}OK${RESET}" || echo -e " ${RED}FAILED${RESET}"
 }
 
 build_dir "Kernel Module" kernel/modules/parasite
@@ -20,4 +20,5 @@ build_dir "libcomponent" lib/libcomponent
 build_dir "libexo" lib/libexo
 build_dir "AHCI device driver" drivers/ahci
 build_dir "Base example component" examples/components/base-example
+build_dir "Dummy block device component" examples/components/block-device
 
