a4e21cef9477fc9aa05e444c3585994ea5462f0a
Changhui Lin <changhui.lin@samsung.com>
2015-02-09 15:36:52 -0800

    fixed bug associated with E_FULL/S_OK

--- drivers/nvme-ssd/nvme_queue.cc
+++ drivers/nvme-ssd/nvme_queue.cc
@@ -111,7 +111,7 @@ status_t NVME_queues_base::increment_submission_tail(queue_ptr_t * tptr) {
 
   /* check if the SQ is full */
   if(_sq_tail + 1 == _sq_head || _sq_tail == _queue_items && _sq_head == 0 ) {
-    return E_FULL;
+    return Exokernel::E_FULL;
   }
 
   *tptr = _sq_tail++;
@@ -243,7 +243,8 @@ Submission_command_slot * NVME_queues_base::next_sub_slot(signed * slot_id) {
   *slot_id = slot + 1;
 
   /* if we get here we know there is no overflow possible */
-  while ( (st = increment_submission_tail(&curr_ptr)) != S_OK );
+  status_t st;
+  while ( (st = increment_submission_tail(&curr_ptr)) != Exokernel::S_OK );
 
   return &_sub_cmd[curr_ptr];
 }
